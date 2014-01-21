e184ae45ab1ba82fd53d531afb52ffac4b8f19b6
cpbaumann <cpbaumann@googlemail.com>
2014-01-21 13:17:28 +0100

    JSHint ed

--- js/jquery.placeholder-for-oldies.js
+++ js/jquery.placeholder-for-oldies.js
@@ -2,61 +2,67 @@
 * Author: cpbaumann
 */
 
-(function($) {
-"use strict";
+(function ($) {
+    "use strict";
     $.fn.extend({
-        placeholderforoldies: function(options) {
-           
-           var defaults = {
-                elementId: "default"
-            };
+        placeholderforoldies: function (options) {
 
-            var option = $.extend(defaults, options);
-            var supportinput = 'placeholder' in document.createElement('input');
-            var supporttextarea = 'placeholder' in document.createElement('textarea');
+            var defaults = { elementId: "default" },
+                option = $.extend(defaults, options),
+                //supportinput = (document.createElement('input').hasOwnProperty('placeholder') !== undefined),
+                //supporttextarea = (document.createElement('textarea').hasOwnProperty('placeholder') !== undefined);
+                supportinput = 'placeholder' in document.createElement('input'),
+                supporttextarea = 'placeholder' in document.createElement('textarea');
 
-            if(supportinput && supporttextarea) {
+
+
+            //console.log('supporttextarea: ' + supporttextarea);
+            //console.log('supportinput: ' + supportinput);
+
+            if (supportinput && supporttextarea) {
                 return this;
             }
 
-            return this.each(function() {
+            return this.each(function () {
 
                 var obj = $(this);
-                if (option.elementId != 'default'){
+                if (option.elementId !== 'default') {
                     var element = $(option.elementId, obj);
-                }else{
+                } else {
                     var element = $('*[placeholder]', obj);
                 }
 
-                element.each(function() {
+                element.each(function () {
 
                     var placeholder = $(this).attr('placeholder');
-                    if($(this).val() == '') {
-                        $(this).val(placeholder);
+
+                    if ($(this).val() === '') {
+                        //console.log('--initial--')
                         $(this).addClass('placeholder');
+                        $(this).val(placeholder);
                     }
 
-                    $(this).click(function() {
-                        if($(this).val() == placeholder){
+                    $(this).click(function () {
+                        if ($(this).val() === placeholder) {
                             $(this).val('');
                             $(this).removeClass('placeholder');
                         }
                     });
 
-                    $(this).blur(function() {
-                        if($(this).val() == ''){
+                    $(this).blur(function () {
+                        if ($(this).val() === '') {
                             $(this).val(placeholder);
                             $(this).addClass('placeholder');
                         }
                     });
 
 
-                    $(this).parents('form').submit(function() {
-                        $(this).find('[placeholder]').each(function() {
-                            if ($(this).val() == $(this).attr('placeholder')) {
+                    $(this).parents('form').submit(function () {
+                        $(this).find('[placeholder]').each(function () {
+                            if ($(this).val() === $(this).attr('placeholder')) {
                                 $(this).val('');
                             }
-                        })
+                        });
                     });
 
                 });
